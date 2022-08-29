9202ee1bb0cb4d9465c865855a037fe4a88652d3
rocenu <iumutdikbasan@gmail.com>
2022-08-29 18:48:36 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+
+public class main {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayiyi giriniz ");
+        int row = inp.nextInt();
+
+
+        for (int i = 1; i <= row; i++) {
+            for (int k = 1; k <= (row - i); k++)
+                System.out.print(" ");
+            for (int j = 1; j <= (2 * i) - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for(int i=row-1;i>=1;i--)
+        {
+            for(int j=1;j<=row-i;j++)
+                System.out.print(" ");
+            for(int j=1;j<=2*i-1;j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
